8b36271ce0c460c42a7213a1b970ba39bb583732
Sem Postma <sem.postma@gmail.com>
2017-12-17 20:22:38 +0100

    prod6

--- src/pages/GraphicDesign.js
+++ src/pages/GraphicDesign.js
@@ -2,7 +2,6 @@ import React, { Component, Dimensions } from 'react';
 import ImageLayout from '../ImageLayout';
 import grey from './grey.png'
 import content from '../content';
-import windowSize from 'react-window-size';
 
 export default class GraphicDesignPage extends Component {
     render() {
